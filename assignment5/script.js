b413499df72ab37a0c8a151a1b3b2169b30f716e
StephanieM04 <smoreno@email.neit.edu>
2022-11-07 14:13:14 -0500

    hwstart

--- assignment5/script.js
+++ assignment5/script.js
@@ -1,7 +1,16 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("can");
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
+
+ctx.fillStyle = "#ffff00";
+ctx.strokeStyle = "red";
+ctx.linewidth = "5";
 ctx.beginPath();
-ctx.arc(x, 300, 20, 0, 2 * Math.PI, true)
+ctx.arc(384, 441, 67, 0, 2 * Math.PI, true)
 ctx.fill()
+
+ctx.fillStyle = "yellow";
+ctx.strokeStyle = "black";
+ctx.linewidth = "5";
+ctx.fillRect(86,300,99,100);
+ctx.strokeRect(86,300,99,100);
