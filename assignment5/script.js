c8e43278e3a6ff9bc83d1d86d7d5364cb81a0634
StephanieM04 <smoreno@email.neit.edu>
2022-11-07 13:50:55 -0500

    canvas js

--- assignment5/script.js
+++ assignment5/script.js
@@ -8,6 +8,3 @@ ctx.fillStyle = "rgb(0,0,255)";
 ctx.strokeStyle = "black";
 ctx.linewidth = "5"; //dont have to specify px or inch
 
-//draws a rectangle: fillRect(x,y,width,height)
-ctx.fillRect(30,30,200,100);
-ctx.strokeRect(300,30,200,100);
